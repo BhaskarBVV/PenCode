7fdc256f0bae0e9c91b911863d10eaa058991b5b
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-09-16 20:03:31 +0530

    Create Delete all nodes with value K (recursive).cpp

---
+++ Linked List/Delete all nodes with value K (recursive).cpp
@@ -0,0 +1,62 @@
+//Bhaskar Varshney
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *BuildList()
+{
+    ListNode *head = NULL, *temp;
+    int choice = 1;
+    while (choice == 1)
+    {
+        ListNode *newnode = (ListNode *)malloc(sizeof(ListNode));
+        cout << "Enter data : ";
+        cin >> newnode->val;
+        newnode->next = 0;
+        if (head == 0)
+            head = temp = newnode;
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+        cout << "Want another node to be inserted : ";
+        cin >> choice;
+    }
+    return head;
+}
+ListNode *removeElements(ListNode *head, int &val)
+{
+    if (head == NULL)
+        return NULL;
+    head->next = removeElements(head->next, val);
+    if (head->val == val)
+        return head->next;
+    return head;
+}
+void print(ListNode *head)
+{
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+int main()
+{
+    cout << "Build list : \n";
+    ListNode *head = BuildList();
+    int x;
+    cout << "Enter the Node value to be deleted : ";
+    cin >> x;
+    head = removeElements(head, x);
+    cout << "\nThe List after removig nodes : ";
+    print(head);
+}
