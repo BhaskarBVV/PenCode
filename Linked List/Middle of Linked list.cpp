a2a369a51c3535661a0d682e9dc5245259e2b2dd
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-09-25 00:48:52 +0530

    Create Middle of Linked list.cpp

---
+++ Linked List/Middle of Linked list.cpp
@@ -0,0 +1,58 @@
+//Bhaskar Varshney
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *BuildList()
+{
+    ListNode *head = NULL, *temp;
+    int choice = 1;
+    while (choice == 1)
+    {
+        ListNode *newnode = (ListNode *)malloc(sizeof(ListNode));
+        cout << "Enter data : ";
+        cin >> newnode->val;
+        newnode->next = 0;
+        if (head == 0)
+            head = temp = newnode;
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+        cout << "Want another node to be inserted : ";
+        cin >> choice;
+    }
+    return head;
+}
+void print(ListNode *head)
+{
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+int mid(ListNode *head)
+{
+    ListNode *slow = head, *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow->val;
+}
+int main()
+{
+    cout << "Build list : \n";
+    ListNode *head = BuildList();
+    cout << "Mid of link list : " << mid(head);
+}
