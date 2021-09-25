f0192d9a7f41e1d59c7d53518212815088429f41
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-09-25 16:05:28 +0530

    Create Count occurrences in Linked List.cpp

---
+++ Recursion/Count occurrences in Linked List.cpp
@@ -0,0 +1,57 @@
+//Bhaskar Varshney
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *BuildList()
+{
+    ListNode *head = NULL, *temp;
+    int choice = 1;
+    while (choice == 1)
+    {
+        ListNode *newnode = (ListNode *)malloc(sizeof(ListNode));
+        cout << "Enter data : ";
+        cin >> newnode->val;
+        newnode->next = 0;
+        if (head == 0)
+            head = temp = newnode;
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+        cout << "Want another node to be inserted : ";
+        cin >> choice;
+    }
+    return head;
+}
+void print(ListNode *head)
+{
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+int count(ListNode *head, int x)
+{
+    if (head == NULL)
+        return 0;
+    return count(head->next, x) + ((head->val == x) ? 1 : 0);
+}
+int main()
+{
+    cout << "Build list : \n";
+    ListNode *head = BuildList();
+    cout << "Enter the numeber to count occurences : ";
+    int x;
+    cin >> x;
+    cout << "The occurences of " << x << " : " << count(head, x);
+}
