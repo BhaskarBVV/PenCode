4a00239b5cae26f168107d2d1be2d299d1f218b2
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-09-30 12:20:20 +0530

    Create Sum of right leaf nodes.cpp

---
+++ Binary Tree/Sum of right leaf nodes.cpp
@@ -0,0 +1,56 @@
+//Bhaskar Varshney
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+TreeNode *BuildTree()
+{
+    int d;
+    cin >> d;
+    if (d == -1)
+        return NULL;
+    TreeNode *Node = new TreeNode(d);
+    cout << "Enter the left child of " << d << " : ";
+    Node->left = BuildTree();
+    cout << "Enter the right child of " << d << " : ";
+    Node->right = BuildTree();
+    return Node;
+}
+void print(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->val;
+    print(root->left);
+    print(root->right);
+}
+void treeHelper(TreeNode *root, int &sum)
+{
+    if (root == NULL)
+        return;
+    if (root->right != NULL && (root->right->left == NULL & root->right->right == NULL))
+        sum += root->right->val;
+    treeHelper(root->left, sum);
+    treeHelper(root->right, sum);
+}
+int leftLeavesSum(TreeNode *root)
+{
+    int sum = 0;
+    treeHelper(root, sum);
+    return sum;
+}
+int main()
+{
+    cout << "Build first tree : ";
+    TreeNode *root = BuildTree();
+    cout << "PreOrder of Tree is: ";
+    print(root);
+    cout << "The sum of left leaf node : " << leftLeavesSum(root);
+}
