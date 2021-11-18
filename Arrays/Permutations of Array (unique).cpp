ff937be849dd8d1d06c2d847fbb9316462df7db5
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-11-18 15:06:12 +0530

    Create Permutations of Array (unique).cpp

---
+++ Arrays/Permutations of Array (unique).cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void backtrack(set<vector<int>>&s, vector<int>nums, vector<int>cur, bool visited[])
+    {
+        if(cur.size()==nums.size())
+        {  
+            s.insert(cur);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(visited[i]==false)
+            {
+                cur.push_back(nums[i]);
+                visited[i]=true;
+                backtrack(s,nums,cur,visited);
+                cur.pop_back();
+                visited[i]=false;
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) 
+    {
+        set<vector<int>>s;
+        vector<vector<int>>res;
+        vector<int>cur;
+        bool visited[nums.size()];
+        // memset(visited, visited+nums.size(), false);
+        for(int i=0;i<nums.size();i++)
+            visited[i]=false;
+        backtrack(s, nums, cur, visited);
+        for(auto it: s)
+            res.push_back(it);
+        return res;
+    }
+};
