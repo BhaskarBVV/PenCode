4c454ecf4aaf68ab01febe816bfebd5cad51a9e9
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-11-18 15:06:52 +0530

    Create Permutations of Array.cpp

---
+++ Arrays/Permutations of Array.cpp
@@ -0,0 +1,37 @@
+class Solution 
+{  
+  // used bactracking 
+  // inserted one by one all the elements in current vector (storing single permutation) and pushed that current vector in my result vector when all the elements are in current vector.
+  // while coming back removed all the popped elements one by one and turned their visited to false;
+  // Its similar to formation on binary tree, i.e, constructed left->left->left->left->.....then came back all way to root.
+public:
+    void backtrack(vector<vector<int>>&res, vector<int>nums,vector<int>cur, bool visited[])
+    {
+        if(cur.size()==nums.size())
+        {
+            res.push_back(cur);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(visited[i]==true)  
+                continue;
+            cur.push_back(nums[i]);
+            visited[i]=true;
+            backtrack(res, nums, cur, visited);
+            cur.pop_back();
+            visited[i]=false;
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<vector<int>>res;
+        vector<int>v;
+        bool visited[nums.size()];
+        // memset(visited, visited+nums.size(), false);
+        for(int i=0;i<nums.size();i++)
+            visited[i]=false;
+        backtrack(res, nums, v, visited);
+        return res;
+    }
+};
