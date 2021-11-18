dddf7877d6a64fa5b476f188663dd7c23e09c656
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-11-18 20:43:26 +0530

    Update Subsets of given array.cpp

--- Arrays/Subsets of given array.cpp
+++ Arrays/Subsets of given array.cpp
@@ -22,3 +22,16 @@ public:
         return res;
     }
 };
+
+// a={1,2,3}
+// Total subsets = 2^n;
+//                                                           Subset
+//    Num    Binary   ->                                     {123}
+//     0      000     ->                                      ---
+//     1      001     ->                                      --3
+//     2      010     ->    Place number in place of 1        -2-
+//     3      011     ->      in binary notation.             -23
+//     4      100     ->                                      1--
+//     5      101     ->                                      1-3
+//     6      110     ->                                      12-
+//     7      111     ->                                      123
