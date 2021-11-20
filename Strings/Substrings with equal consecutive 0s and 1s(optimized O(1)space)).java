bb188e9cc27f4726ea8f6d4d5071ee5dbaf6e722
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-11-20 15:55:58 +0530

    Create Substrings with equal consecutive 0s and 1s(optimized O(1)space)).java

---
+++ Strings/Substrings with equal consecutive 0s and 1s(optimized O(1)space)).java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int countBinarySubstrings(String s) 
+    {
+        String temp="";
+        int pre_cnt=0, cnt=0,tot=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(temp=="")
+            {
+                temp=""+s.charAt(i);cnt=1;
+            }
+            else if(temp.charAt(0)!=s.charAt(i))
+            {
+                tot+=Math.min(pre_cnt,cnt);
+                temp=""+s.charAt(i);
+                pre_cnt=cnt;
+                cnt=1;
+            }
+            else
+            {
+                temp+=s.charAt(i);
+                cnt++;
+            }
+        }
+        tot+=Math.min(pre_cnt,cnt);
+        return tot;
+    }
+}
