92f8a3e5028c7944a098d5c909faeda6103caa13
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-11-23 15:01:56 +0530

    Update Total unique BST's for n nodes.java

--- Binary Search Tree/Total unique BST's for n nodes.java
+++ Binary Search Tree/Total unique BST's for n nodes.java
@@ -20,3 +20,33 @@ class Solution {
         return dp[n];
     }
 }
+
+
+// Nodes-----------------BST's
+// ->  0      -----       1
+// ->  1      -----       1
+    
+// ->  2      -----    f(0)*f(1)+f(1)*f(0)
+//  ex-10,20
+//     10               20
+//    /  \            /   \
+//   x    20         10    x
+// f(0) * f(1)  +  f(1) * f(0)
+    
+// ->  3      -----    f(0)*f(2) + f(1)*f(1) + f(2)*f(0)
+// ex-10,20,30
+//     10                20                30      
+//    /  \              /  \             /   \
+//   x  20,30          10  30         10,20   x
+//     we know                       we know 
+//     the number                   the number
+//     of BST for                   of BST for 
+//     2 BST's                        2 BST's
+//  f(0)*f(2)  +    f(1) * f(1)   +   f(2) * f(0)
+        
+// -> 4     ------     f(0)*f(3) + f(1)*f(2) + f(2)*f(1) + f(3)*f(0)
+//                     left value increases and right value decreases.
+//                     left = 0 -> n-1 and right = n-1 -> 0.
+
+
+// similar to catalan numbers...
