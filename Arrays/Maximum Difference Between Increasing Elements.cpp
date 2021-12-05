4dfdcce5ef65967aecec3f1d95b5193bd3f2d5ca
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2021-12-05 21:58:43 +0530

    Update Maximum Difference Between Increasing Elements.cpp

--- Arrays/Maximum Difference Between Increasing Elements.cpp
+++ Arrays/Maximum Difference Between Increasing Elements.cpp
@@ -1,3 +1,19 @@
+// Input: nums = [7,1,5,4]
+// Output: 4
+// Explanation:
+// The maximum difference occurs with i = 1 and j = 2, nums[j] - nums[i] = 5 - 1 = 4.
+// Note that with i = 1 and j = 0, the difference nums[j] - nums[i] = 7 - 1 = 6, but i > j, so it is not valid.
+
+// Input: nums = [9,4,3,2]
+// Output: -1
+// Explanation:
+// There is no i and j such that i < j and nums[i] < nums[j].
+
+// Input: nums = [1,5,2,10]
+// Output: 9
+// Explanation:
+// The maximum difference occurs with i = 0 and j = 3, nums[j] - nums[i] = 10 - 1 = 9.
+
 class Solution {
 public:
     int maximumDifference(vector<int>& a)
