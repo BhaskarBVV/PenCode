57715374c10535fe6e5a58ac963da46c883549d6
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2022-02-24 00:50:22 +0530

    Create Prims.cpp

---
+++ Graphs/Prims.cpp
@@ -0,0 +1,56 @@
+#define ll long long int
+class Solution
+{
+public:
+    struct comp
+    {
+        bool operator()(pair<int, int> const &p1, pair<int, int> const &p2)
+        {
+            return p1.second > p2.second;
+        }
+    };
+    int minCostConnectPoints(vector<vector<int>> &points)
+    {
+        int n = points.size();
+        vector<pair<int, int>> adj[n];
+        for (int i = 0; i < points.size(); i++)
+            for (int j = i + 1; j < points.size(); j++)
+            {
+                int x1 = points[i][0], x2 = points[j][0], y1 = points[i][1], y2 = points[j][1];
+                int d = abs(x1 - x2) + abs(y1 - y2);
+                adj[i].push_back({j, d});
+                adj[j].push_back({i, d});
+            }
+
+        vector<ll> val(n, LONG_LONG_MAX);
+        vector<int> parent(n + 1, INT_MAX);
+        parent[0] = -1;
+        val[0] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq;
+        vector<bool> vis(n, false);
+        pq.push({0, 0});
+        while (!pq.empty())
+        {
+            pair<int, int> temp = pq.top();
+            pq.pop();
+            vis[temp.first] = true;
+            for (auto it : adj[temp.first])
+            {
+                if (vis[it.first] == false)
+                {
+                    if (it.second < val[it.first])
+                    {
+                        val[it.first] = it.second;
+                        pq.push({it.first, val[it.first]});
+                    }
+                }
+            }
+        }
+        vector<int> ans;
+        int sum=0;
+        reverse(ans.begin(), ans.end());
+        for (auto it : val)
+            sum+=it;
+        return sum;
+    }
+};
