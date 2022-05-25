f6f6a618db0883f0c9366faa6efb78cd3d083f26
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2022-05-25 21:53:15 +0530

    Update Subarrays with sum K.cpp

--- Arrays/Subarrays with sum K.cpp
+++ Arrays/Subarrays with sum K.cpp
@@ -24,3 +24,32 @@ int findSubArraySum(int a[], int n, int k)
         }
         return cnt;
     }
+
+
+
+
+// OR
+
+class Solution{
+    public:
+    int findSubArraySum(int Arr[], int N, int k)
+    {
+        map<int,int>mp;
+        int ans=0,sum=0;
+
+        mp[0]=1;
+        
+        for(int i=0;i<N;i++)
+        {
+            sum+=Arr[i];
+
+            if(mp.find(sum-k)!=mp.end())
+            ans+=mp[sum-k];
+
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
+
+
