5e7c2bb51a16945867b85883c603a6ff49ea7f3e
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2024-10-14 19:15:00 +0530

    Create 3 Sum.cpp

---
+++ Quick Revamp/Arrays/3 Sum.cpp
@@ -0,0 +1,42 @@
+https://leetcode.com/problems/3sum/description/
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int requried_sum = 0 - nums[i];
+
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right)
+            {
+                int sum = nums[left] + nums[right];
+                if (sum == requried_sum)
+                {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+                    left++;
+                    right--;
+                }
+                else if (sum > requried_sum)
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+        }
+        return ans;
+    }
+};
